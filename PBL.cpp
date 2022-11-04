09f3e5e486c213093298bcc6a2929394fe128944
vn242 <109225115+vn242@users.noreply.github.com>
2022-11-04 09:38:32 +0700

    Update PBL.cpp

--- PBL.cpp
+++ PBL.cpp
@@ -5,40 +5,8 @@ using namespace std;
 int main()
 {
 	srand(time(NULL));
-	//menu_muahang();
-	//menu21();
-	//ds_kh p;
-	//p.docFile();
-	//cout << p.timSdt("3124");
-	//ds_dh d;
-	//don_hang p;
-	//string a = " ";
-	//d.themdh(p, a);
-	//menu23("0818368242");
-	//menu_chinh();
-	   //menu();
-		//menu_damuahang();
-	//_getch();
-	//menu12();
-	//menu_hoadon("DH5086");
-	//menu_chinh();
-	//menu_ctdh();
-	//menu23("0818368242");
-	//menu_qlhd();
-	//ds_nv t;
-	//t.docFile();
-	//t.nhap();
-	//t.ghiFile();
-	//menu_themnv();
-	//t.docFile();
-	//t.in();
-	//t.ghiFile();
-	//menu_themnv();
-	//menu_ctdh();
 	menu_chinh();
-	//menu_chuamuahang();
 	_getch();
-	// inHD("DH5086");
 
 }
 
